d3089e342ea3d8df720632cf09dd1b0acf609de6
Victor Soares <vitorsoares96@hotmail.com>
2022-01-03 15:22:50 -0300

    x

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 
 import { store } from './store';
 
@@ -10,17 +10,31 @@ const dynamicFederation = async (scope, module) => {
   await container.init(__webpack_share_scopes__.default);
   return container.get(module).then((factory) => {
     const Module = factory();
+    console.log('Module', Module);
     return Module;
   });
 };
 
 const RemoteApp = React.lazy(() => dynamicFederation('app2', './RemoteApp'));
 
+const Inner = () => {
+  const state = useSelector((state) => state);
+  return (
+    <div>
+      Welcome to Host App
+      state: {JSON.stringify(state)}
+
+      <button onClick={() => store.dispatch({ type: 'counter/increment' })}>
+        Increment
+      </button>
+    </div>
+  );
+}
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        Welcome to Host App
+        <Inner />  
         <div>
           <Suspense fallback="Loading...">
             <RemoteApp store={store} />
